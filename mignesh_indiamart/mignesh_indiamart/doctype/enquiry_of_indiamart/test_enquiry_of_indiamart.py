9b9d3efc74c5798138fb2e036a05c0eb99e27ade
Dhruvipatel12 <dhruvi.kaneriya@sanskartechnolab.in>
2023-03-28 13:04:09 +0530

    Create Doctype

---
+++ mignesh_indiamart/mignesh_indiamart/doctype/enquiry_of_indiamart/test_enquiry_of_indiamart.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Dhruvi and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestEnquiryofIndiamart(FrappeTestCase):
+	pass
